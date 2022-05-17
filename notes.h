61ab86499247fdfa2d1f331ef0ab288d61a6c4fe
Spencer <madridium16@gmail.com>
2022-05-16 17:05:25 -0700

    first commit

---
+++ notes.h
@@ -0,0 +1,13 @@
+// frequencies from 3rd/4th scale
+#define noteA 220
+#define noteAS 233
+#define noteB 247
+#define noteC  262
+#define noteCS 277
+#define noteD  294
+#define noteDS 311
+#define noteE  330
+#define noteF  349
+#define noteFS 370
+#define noteG 392
+#define noteGS 415
---
+++ speaker.ino
@@ -0,0 +1,42 @@
+#include <PCM.h>
+
+// stores frequency values for different notes
+#include"notes.h"
+
+// creates references to pin numbers and defines constants
+#define trigPin 13
+#define echoPin 12
+#define speaker 11 
+#define spd 0.0343 //speed of sound in cm/s
+#define offset 4 // offsets the position 0 for the sensor
+#define interval 10 // length of each key
+
+int notes[] = {noteA, noteB, noteC, noteD, noteE, noteF, noteG}; // array of note frequencies
+
+void setup() {
+  Serial.begin (9600);
+  // trigger pin sends signal based on distance 
+  pinMode(trigPin, OUTPUT);
+  // echo pin sends signal when reflected wave is recieved
+  pinMode(echoPin, INPUT);
+
+} 
+
+void loop() {
+  long duration, distance; // initializes duration and distance variable 
+  digitalWrite(trigPin, LOW); // sets trigPin to LOW
+  delayMicroseconds(2); // must be some delay
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW); 
+  duration = pulseIn(echoPin, HIGH); // sets duration as time for sound to travel back and forth
+  distance = (spd*duration)/2; // sets distance as the one-way distance for the sound, spd is in cm/s, duration in s
+  noTone(speaker); // turns off speaker if not already off 
+
+  
+  if (distance >= offset && distance <= interval * sizeof(notes) + offset) { // checks that the detection is within range
+    tone(speaker, notes[(distance - offset)/interval] ); // plays frequency whose value is based on the distance from the sensor
+  } 
+  
+  delay(1000); 
+}
